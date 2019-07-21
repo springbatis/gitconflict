f30a3edc532d9276320881ee28381c5ffecebc7d
zhangsan <zhangsan@itcast.cn>
2019-07-21 13:32:58 +0800

    b add finally

---
+++ src/com/chun/Demo.java
@@ -0,0 +1,16 @@
+package com.chun;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 1;
+		a=a+1;
+		int b = 3;
+		int c = 3;
+		System.out.println(c);
+		System.out.println("finally");
+		System.out.println("finally");
+	}
+
+}
