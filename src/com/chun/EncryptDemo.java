6528cad00ea4ab5e9e9b884c391fa22b1501fa55
zhangsan <zhangsan@itcast.cn>
2020-12-30 22:11:26 +0800

    fix

---
+++ src/com/chun/EncryptDemo.java
@@ -0,0 +1,46 @@
+package com.chun;
+
+import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.UnsupportedEncodingException;
+import java.net.ConnectException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class EncryptDemo {
+    public static byte[] encrypt(String content,String password) {
+        try {
+            KeyGenerator kgen = KeyGenerator.getInstance("AES");
+            kgen.init(128, new SecureRandom(password.getBytes()));
+            SecretKey secretKey = kgen.generateKey();
+            byte[] enCodeFormat = secretKey.getEncoded();
+            SecretKeySpec key = new SecretKeySpec(enCodeFormat, "AES");
+            Cipher cipher = Cipher.getInstance("AES");
+            byte[] byteContent = content.getBytes("utf-8");
+            cipher.init(Cipher.ENCRYPT_MODE,key);
+            byte[] result = cipher.doFinal(byteContent);
+            return result;
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String content = "huawei@123";
+        String workey = "adfdkjkjdfefe:12kjkdf3dfddfdfdfd";
+        byte[] encrypt = encrypt(content, workey);
+        System.out.println(new String(encrypt));
+    }
+}
