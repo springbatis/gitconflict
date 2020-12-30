416ba59d978f57fe75dd292db8a0f35370ebc34b
zhangsan <zhangsan@itcast.cn>
2020-12-30 22:14:18 +0800

    update content

--- src/com/chun/EncryptDemo.java
+++ src/com/chun/EncryptDemo.java
@@ -38,7 +38,7 @@ public class EncryptDemo {
     }
 
     public static void main(String[] args) {
-        String content = "huawei@123";
+        String content = "ddd@123";
         String workey = "adfdkjkjdfefe:12kjkdf3dfddfdfdfd";
         byte[] encrypt = encrypt(content, workey);
         System.out.println(new String(encrypt));
